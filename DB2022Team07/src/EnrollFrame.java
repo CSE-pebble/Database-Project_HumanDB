c0c63fd77370e8da5f1b303afbeec1155f1221be
seondal <sseondal@gmail.com>
2022-06-05 02:11:08 +0900

    [Feat] EnrollFrame 최종
    - start_date 세개의 필드로 나눠서 입력받기
    - 코드 리팩토링.... 일단....

--- DB2022Team07/src/EnrollFrame.java
+++ DB2022Team07/src/EnrollFrame.java
@@ -14,122 +14,77 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
-import javax.swing.text.AttributeSet.ColorAttribute;
 
-import java.sql.Date;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class EnrollFrame extends JFrame {
 	
+	private Vector<Integer> id = new Vector<Integer>();
+	private String member_id="";
+		
+	// 현재 날짜 받아와서 포매팅 
 	LocalDate now = LocalDate.now();
 	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 	String formatedNow = now.format(formatter);
 	java.sql.Date today = java.sql.Date.valueOf(formatedNow);
 	
+	
+	// 로그인 요소 
 	private JTextField name_field = new JTextField(25);
 	private JTextField phone_field = new JTextField(25);
 	private JTextField passwd_field = new JTextField(25);
 	
-	private JTextField membership_field = new JTextField(4);
-	private JTextField startDate_field = new JTextField(8);
+	private JLabel sign_in_result = new JLabel();
+	private JLabel membership_search_result = new JLabel();
 	
-	private JButton close = new JButton("close");
-	private JButton btn = new JButton("OK");
-	private JLabel t1 = new JLabel();
-	private JLabel t2 = new JLabel();
-	private JLabel t3 = new JLabel();
-	private JLabel t3_1 = new JLabel();
-	private JLabel t4 = new JLabel();
-	private Vector<Integer> id = new Vector<Integer>();
-	private String member_id="";
+	private JButton signInBtn = new JButton("Sign In");
 
+	
+	// 회원권 등록 요소 
+	private JLabel membership_number_insert = new JLabel();
+	private JTextField membership_field = new JTextField(4);
+	
+	private JLabel start_date_insert = new JLabel();
+	private JLabel year = new JLabel("Year");
+	private JLabel month = new JLabel("Month");
+	private JLabel day = new JLabel("Day");
+	private JTextField year_field = new JTextField(4);
+	private JTextField month_field = new JTextField(2);
+	private JTextField day_field = new JTextField(2);
+	
+	private JButton enrollBtn = new JButton("Enroll");
+	private JLabel membership_enroll_result = new JLabel();
+
+
+	
 	public EnrollFrame() {
 		setTitle("Enroll Membership");
 		
-		membership_field.setVisible(false);
-		startDate_field.setVisible(false);
-		close.setVisible(false);
+		showEnrollField(false);
 		
 		id.clear();
 		
-		name_field.setText("");
-		phone_field.setText("");
-		passwd_field.setText("");
-		membership_field.setText("");
-		startDate_field.setText("");
+		// 로그인 및 회원권 조회 
+		signInBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String name = name_field.getText();
+				String phone = phone_field.getText();
+				String passwd = passwd_field.getText();
+				
+				signIn(name, phone, passwd);
+			}
+		});
 		
-		btn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				String name = name_field.getText().trim()==null?"":name_field.getText().trim();
-				String phone = phone_field.getText().trim()==null?"":phone_field.getText().replaceAll("-","").trim();
-				String passwd = passwd_field.getText().trim()==null?"":passwd_field.getText().trim();
-				BuyMembership(name, phone, passwd);
-			}
-		});
-		close.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				name_field.setText("");
-				phone_field.setText("");
-				passwd_field.setText("");
-				membership_field.setText("");
-				
-				t1.setText("");
-				t2.setText("");
-				t3.setText("");
-				t3_1.setText("");
-				t4.setText("");
-				
-				member_id = null;
-				
-				dispose();
-			}
-		});
-		membership_field.addActionListener(new ActionListener() {
+		// 회원권 등록 
+		enrollBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String membership_id = membership_field.getText().trim()==null?"":membership_field.getText().trim();
-				String start_date_inserted = startDate_field.getText();
+				String start_date_inserted = year_field.getText() + "-" + month_field.getText() + "-" + day_field.getText();
 				java.sql.Date start_date = java.sql.Date.valueOf(start_date_inserted);
 				
-				// 조회되지 않는 회원권인 경우 
-				if (! id.contains(Integer.parseInt(membership_id))) {
-					t4.setText("<html><body style='text-align:center;'>"
-							+ "Wrong Membership number!<br />"
-							+ "Enter it again.<br/>"
-							+ "----------------------------------------------------------------------------------<br/>"
-							+ "</body></html>");
-				}
-				// 시작날짜가 등록날짜(오늘) 이전인 경우 
-				else if (start_date.before(today)) {
-					t4.setText("<html><body style='text-align:center;'>"
-							+ "Start Date Must be After Today(" + formatedNow + "). <br/>" 
-							+ "Enter it again.<br/>"
-							+ "----------------------------------------------------------------------------------<br/>"
-							+ "</body></html>");
-				}
-				else {
-					try (Connection conn = DriverManager.getConnection(
-							"jdbc:mysql://localhost:3306/" + DB2022Team07_main.DBID, DB2022Team07_main.USERID,
-							DB2022Team07_main.PASSWD); Statement stmt = conn.createStatement();) {
-						PreparedStatement pStmt = conn
-								.prepareStatement("insert into db2022_enroll values(?,?,?,?);");
-						pStmt.setString(1, member_id);
-						pStmt.setDate(2, today);
-						pStmt.setDate(3, start_date);
-						pStmt.setString(4, membership_id);
+				enrollMembership(start_date, membership_id);
 
-						pStmt.executeUpdate();
-						t4.setText("<html><body style='text-align:center;'>"
-								+ "Membership Registration Completed.<br/>"
-								+ "----------------------------------------------------------------------------------<br/>"
-								+ "</body></html>");
-						close.setVisible(true);
-					} catch (SQLException sqle) {
-						System.out.println("SQL Exception: " + sqle);
-					}
-				}
 			}
 		});
 		
@@ -146,77 +101,135 @@ public class EnrollFrame extends JFrame {
 		contentPane.add(phone_field);
 		contentPane.add(new JLabel(" Password "));
 		contentPane.add(passwd_field);
-		contentPane.add(btn);
-		contentPane.add(t1);
-		contentPane.add(t2);
-		contentPane.add(t3);
+		contentPane.add(signInBtn);
+		contentPane.add(sign_in_result);
+		
+		contentPane.add(membership_search_result);
+		contentPane.add(membership_number_insert);
 		contentPane.add(membership_field);
-		contentPane.add(t3_1);
-		contentPane.add(startDate_field);
-		contentPane.add(t4);
-		contentPane.add(close);
-
-		setSize(350, 500);
+		contentPane.add(start_date_insert);
+		contentPane.add(year);
+		contentPane.add(year_field);
+		contentPane.add(month);
+		contentPane.add(month_field);
+		contentPane.add(day);
+		contentPane.add(day_field);
+		contentPane.add(membership_enroll_result);
+		contentPane.add(new JLabel("<html><body style='text-align:center;'>----------------------------------------------------------------------------------<br/></body></html>"));
+		contentPane.add(enrollBtn);
+		
+		setSize(350, 700);
 		setVisible(true);
 	}
 
-	void BuyMembership(String member_name, String phone, String passwd) {
+	void signIn(String member_name, String phone, String passwd) {
 		try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + DB2022Team07_main.DBID,
 				DB2022Team07_main.USERID, DB2022Team07_main.PASSWD); Statement stmt = conn.createStatement();) {
 			
-			PreparedStatement pStmt = conn.prepareStatement(
-					"select * from DB2022_members where name=? and phone=?;");
+			PreparedStatement pStmt = conn.prepareStatement("select * from DB2022_members where name=? and phone=?;");
 			pStmt.setString(1, member_name);
 			pStmt.setString(2, phone);
 			ResultSet info_set = pStmt.executeQuery();
 			
+			// 회원가입이 되지 않은 멤버인 경우 
 			if (!info_set.next()) {
-				t1.setText("<html><body style='text-align:center;'>"
+				sign_in_result.setText("<html><body style='text-align:center;'>"
 						+ "----------------------------------------------------------------------------------<br/>"
 						+ "Member does not exist.<br/>"
 						+ "----------------------------------------------------------------------------------"
 						+ "</body></html>");
 			} else {
 				
+				// 비밀번호가 일치하지 않은 경우 
 				if (!passwd.equals(info_set.getString("password"))) {
-					t1.setText("<html><body style='text-align:center;'>"
+					sign_in_result.setText("<html><body style='text-align:center;'>"
 							+ "----------------------------------------------------------------------------------<br/>"
 							+ "Wrong password!<br/>"
 							+ "----------------------------------------------------------------------------------</body></html>");
 				} else {
-					
+					// 정상 로그인
 					member_id = info_set.getString("member_id");
 
 					pStmt = conn.prepareStatement("select * from DB2022_membership;\r\n");
 					
 					ResultSet membership_set = pStmt.executeQuery();
 
-					t1.setText("<html><body style='text-align:center;'>"
+					sign_in_result.setText("<html><body style='text-align:center;'>"
 							+ "----------------------------------------------------------------------------------<br/>"
 							+ "This is a list of Memberships Available.<br /> "
 							+ "</body></html>");
-					t2.setText("<html><body style='text-align:center;'>");
+					membership_search_result.setText("<html><body style='text-align:center;'>");
 					
 					while (membership_set.next()) {
 						id.add(membership_set.getInt("membership_id"));
-						t2.setText(t2.getText() + "[" + membership_set.getString("membership_id")+"]" + " "
+						membership_search_result.setText(membership_search_result.getText() + "[" + membership_set.getString("membership_id")+"]" + " "
 								+ membership_set.getString("name") + ", Period: " + membership_set.getString("period") + " price "
 								+ membership_set.getString("price") + " won<br />");
 
 					}
-					t2.setText(t2.getText() + "----------------------------------------------------------------------------------</body></html>");
 					
-					t3.setText("<html><body style='text-align:center;'>Please choose Membership number: </body></html>");
-					t3_1.setText("<html><body style='text-align:center;'>When will you start using this Membership?: </body></html>");
-
-					membership_field.setVisible(true);
-					startDate_field.setVisible(true);
-				}
+					// 로그인 이후 화면 변환
+					membership_search_result.setText(membership_search_result.getText() + "----------------------------------------------------------------------------------</body></html>");					
+					membership_number_insert.setText("<html><body style='text-align:center;'>Please choose Membership number: </body></html>");
+					start_date_insert.setText("<html><body style='text-align:center;'> When will you start to use this Membership? ----- </body></html>");
+					
+					showEnrollField(true);
+					}
 			}
 
 		} catch (SQLException sqle) {
 			System.out.println("SQLException: " + sqle);
 		}
 
+	}
+	
+	void enrollMembership(java.sql.Date start_date, String membership_id) {
+		// 조회되지 않는 회원권인 경우 
+		if (! id.contains(Integer.parseInt(membership_id))) {
+			membership_enroll_result.setText("<html><body style='text-align:center;'>"
+					+ "Wrong Membership number!<br />"
+					+ "Enter it again.<br/>"
+					+ "----------------------------------------------------------------------------------<br/>"
+					+ "</body></html>");
+		}
+		// 시작날짜가 등록날짜(오늘) 이전인 경우 
+		else if (start_date.before(today)) {
+			membership_enroll_result.setText("<html><body style='text-align:center;'>"
+					+ "Start Date Must be After Today(" + formatedNow + "). <br/>" 
+					+ "Enter it again.<br/>"
+					+ "----------------------------------------------------------------------------------<br/>"
+					+ "</body></html>");
+		}
+		else {
+			try (Connection conn = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/" + DB2022Team07_main.DBID, DB2022Team07_main.USERID,
+					DB2022Team07_main.PASSWD); Statement stmt = conn.createStatement();) {
+				PreparedStatement pStmt = conn
+						.prepareStatement("insert into db2022_enroll values(?,?,?,?);");
+				pStmt.setString(1, member_id);
+				pStmt.setDate(2, today);
+				pStmt.setDate(3, start_date);
+				pStmt.setString(4, membership_id);
+
+				pStmt.executeUpdate();
+				membership_enroll_result.setText("<html><body style='text-align:center;'>"
+						+ "Membership Registration Completed.<br/>"
+						+ "----------------------------------------------------------------------------------<br/>"
+						+ "</body></html>");
+			} catch (SQLException sqle) {
+				System.out.println("SQL Exception: " + sqle);
+			}
+		}
+	}
+	
+	void showEnrollField (Boolean b) {
+		membership_field.setVisible(b);
+		year.setVisible(b);
+		month.setVisible(b);
+		day.setVisible(b);
+		year_field.setVisible(b);
+		month_field.setVisible(b);
+		day_field.setVisible(b);
+		enrollBtn.setVisible(b);
 	}
 }
