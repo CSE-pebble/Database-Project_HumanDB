c44ca68662aa915579ea06a62fe7cb30c7996772
Yongyeon Kim <70844774+yyeonkim@users.noreply.github.com>
2022-06-05 08:01:16 +0900

    Update comments

--- DB2022Team07/src/RegisterFrame.java
+++ DB2022Team07/src/RegisterFrame.java
@@ -18,14 +18,14 @@ public class RegisterFrame extends JFrame {
 	JLabel register = new JLabel("Register completed");
 	Vector<String> branch_vec = new Vector<String>();
 
-	// �쉶�썝媛��엯 GUI瑜� �깮�꽦�븳�떎.
-	// �엯�젰媛믪씠 �쑀�슚�븳吏� �뙋�떒�븯怨� �쉶�썝媛��엯�쓣 吏꾪뻾�븳�떎.
+	// 회원가입 GUI를 생성한다.
+	// 입력값이 유효한지 판단하고 회원가입을 진행한다.
 	public RegisterFrame() {
 		setTitle("Sign Up");
 		Container content = getContentPane();
 		content.setLayout(new FlowLayout());
 		
-		// �븘�뱶 媛� 珥덇린�솕
+		// 필드 값 초기화
 		name_field.setText("");
 		phone_field.setText("");
 		password_field.setText("");
@@ -98,7 +98,7 @@ public class RegisterFrame extends JFrame {
 				String weight = weight_field.getText().trim();
 				String branch = branch_field.getText().trim();
 				
-				//�쟾�솕踰덊샇 �쑀�슚�꽦 寃��궗
+				// 전화번호 유효성 검사
 				boolean phone_flag=true;
 				
 				if(phone.length()!=11) phone_flag=false;
@@ -109,9 +109,9 @@ public class RegisterFrame extends JFrame {
 					}
 				}
 				
-				// �쉶�썝 媛��엯 �떆 湲곗엯�븳 �젙蹂� �옒紐삳릺硫� �뿉�윭 硫붿떆吏� �쓣�슦湲�
+				// 회원 가입 시 기입한 정보 잘못되면 에러 메시지 띄우기
 
-				// 鍮� 媛믪쓣 �꽆湲곕㈃ �뿉�윭 硫붿떆吏� �쓣�슦湲�
+				// 빈 값을 넘기면 에러 메시지 띄우기
 				if (name.isEmpty() || phone.isEmpty() || passwd.isEmpty() || gender.isEmpty() || height.isEmpty()
 						|| weight.isEmpty() || branch.isEmpty()) {
 					label.setText("<html><body style='text-align:center;'>"
@@ -120,27 +120,28 @@ public class RegisterFrame extends JFrame {
 							+ "----------------------------------------------------------------------------------<br/>"
 							+ "</body></html>");
 				} else if (!phone_flag) {
+					// 전화번호가 유효하지 않으면
 					label.setText("<html><body style='text-align:center;'>"
 							+ "----------------------------------------------------------------------------------<br/>"
 							+ "Invalid phone number.<br/>" + "Please enter it again.<br/>"
 							+ "----------------------------------------------------------------------------------<br/>"
 							+ "</body></html>");	
 				} else if (passwd.length() != 4) {
-					// 鍮꾨�踰덊샇媛� 4�옄由ш� �븘�땲硫� �뿉�윭 硫붿떆吏� �쓣�슦湲�
+					// 비밀번호가 4자리가 아니면 에러 메시지 띄우기
 					label.setText("<html><body style='text-align:center;'>"
 							+ "----------------------------------------------------------------------------------<br/>"
 							+ "The password is four digits.<br/>" + "Please enter it again.<br/>"
 							+ "----------------------------------------------------------------------------------<br/>"
 							+ "</body></html>");
 				} else if (!gender.equals("F") && !gender.equals("M")) {
-					// �꽦蹂꾩씠 F/M �쇅�쓽 媛믪씠硫� �뿉�윭 硫붿떆吏� �쓣�슦湲�
+					// 성별이 F/M 외의 값이면 에러 메시지 띄우기
 					label.setText("<html><body style='text-align:center;'>"
 							+ "----------------------------------------------------------------------------------<br/>"
 							+ "Gender must be either F or M.<br/>" + "Please enter it again.<br/>"
 							+ "----------------------------------------------------------------------------------<br/>"
 							+ "</body></html>");
 				} else if (!branch_vec.contains(branch)) {
-					// �뾾�뒗 吏��젏�쓣 �엯�젰�븯硫� �뿉�윭 硫붿떆吏� �쓣�슦湲�
+					// 없는 지점을 입력하면 에러 메시지 띄우기
 					label.setText("<html><body style='text-align:center;'>"
 							+ "----------------------------------------------------------------------------------<br/>"
 							+ "Invalid branch name.<br/>" + "Please enter it again.<br/>"
@@ -154,17 +155,17 @@ public class RegisterFrame extends JFrame {
 
 	}
 
-	// �쑀�슚�븳 �궗�슜�옄 �젙蹂대�� DB�뿉 異붽��븯怨� �쉶�썝媛��엯�쓣 �셿猷뚰븳�떎.
+	// 유효한 사용자 정보를 DB에 추가하고 회원가입을 완료한다.
 	void Register(String name, String phone, String passwd, String gender, String height, String weight,
 			String branch) {
 		try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + DB2022Team07_main.DBID,
 				DB2022Team07_main.USERID, DB2022Team07_main.PASSWD); Statement stmt = conn.createStatement();) {
-			// member table�뿉 tuple瑜� �븯�굹 �궫�엯�븳�떎
+			// member table에 tuple를 하나 삽입한다
 			PreparedStatement pStmt = conn.prepareStatement("insert into DB2022_members values(?,?,?,?,?,?,?,?,?)");
 			ResultSet rSet = stmt.executeQuery("select count(*) as num from DB2022_members;");
 
 			if (rSet.next()) {
-				int n = rSet.getInt("num") + 1; // 珥� �쉶�썝 �닔�뿉 1�쓣 �뜑�븯�뿬 member_id濡� 吏��젙�븳�떎.
+				int n = rSet.getInt("num") + 1; // 총 회원 수에 1을 더하여 member_id로 지정한다.
 				System.out.println(n);
 				pStmt.setInt(1, n);
 				pStmt.setString(2, name);
@@ -176,7 +177,7 @@ public class RegisterFrame extends JFrame {
 				pStmt.setString(8, branch);
 				pStmt.setString(9, null);
 				pStmt.executeUpdate();
-				// �쉶�썝媛��엯 �셿猷� 硫붿떆吏� �쓣�슦湲�
+				// 회원가입 완료 메시지 띄우기
 				label.setText("<html><body style='text-align:center;'>"
 						+ "----------------------------------------------------------------------------------<br/>"
 						+ "Your registration is completed<br/>"
