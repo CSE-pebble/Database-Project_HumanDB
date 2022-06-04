c3f53c1570a95a7c62c4aa21f1d2c9cf7e496f18
Yongyeon Kim <70844774+yyeonkim@users.noreply.github.com>
2022-06-04 23:05:45 +0900

    Update comments

--- DB2022Team07/src/RegisterFrame.java
+++ DB2022Team07/src/RegisterFrame.java
@@ -17,12 +17,15 @@ public class RegisterFrame extends JFrame {
 	JLabel label = new JLabel();
 	JLabel register = new JLabel("Register completed");
 	Vector<String> branch_vec = new Vector<String>();
-
+	
+	// 회원가입 GUI를 생성한다.
+	// 입력값이 유효한지 판단하고 회원가입을 진행한다.
 	public RegisterFrame() {
 		setTitle("Sign Up");
 		Container content = getContentPane();
 		content.setLayout(new FlowLayout());
-
+		
+		// 필드 값 초기화
 		name_field.setText("");
 		phone_field.setText("");
 		password_field.setText("");
@@ -94,7 +97,8 @@ public class RegisterFrame extends JFrame {
 				String height = height_field.getText().trim();
 				String weight = weight_field.getText().trim();
 				String branch = branch_field.getText().trim();
-
+				
+				// 전화번호 형식이 잘못되면 에러 메시지 띄우기
 				try {
 					int phone_num = Integer.parseInt(phone);
 				} catch (NumberFormatException numException) {
@@ -104,7 +108,7 @@ public class RegisterFrame extends JFrame {
 							+ "----------------------------------------------------------------------------------<br/>"
 							+ "</body></html>");
 				} finally {
-
+					// 빈 값을 넘기면 에러 메시지 띄우기
 					if (name.isEmpty() || phone.isEmpty() || passwd.isEmpty() || gender.isEmpty() || height.isEmpty()
 							|| weight.isEmpty() || branch.isEmpty()) {
 						label.setText("<html><body style='text-align:center;'>"
@@ -113,18 +117,21 @@ public class RegisterFrame extends JFrame {
 								+ "----------------------------------------------------------------------------------<br/>"
 								+ "</body></html>");
 					} else if (passwd.length() != 4) {
+						// 비밀번호가 4자리가 아니면 에러 메시지 띄우기
 						label.setText("<html><body style='text-align:center;'>"
 								+ "----------------------------------------------------------------------------------<br/>"
 								+ "The password is four digits.<br/>" + "Please enter it again.<br/>"
 								+ "----------------------------------------------------------------------------------<br/>"
 								+ "</body></html>");
 					} else if (!gender.equals("F") && !gender.equals("M")) {
+						// 성별이 F/M 외의 값이면 에러 메시지 띄우기
 						label.setText("<html><body style='text-align:center;'>"
 								+ "----------------------------------------------------------------------------------<br/>"
 								+ "Gender must be either F or M.<br/>" + "Please enter it again.<br/>"
 								+ "----------------------------------------------------------------------------------<br/>"
 								+ "</body></html>");
 					} else if (!branch_vec.contains(branch)) {
+						// 없는 지점을 입력하면 에러 메시지 띄우기
 						label.setText("<html><body style='text-align:center;'>"
 								+ "----------------------------------------------------------------------------------<br/>"
 								+ "Invalid branch name.<br/>" + "Please enter it again.<br/>"
@@ -138,17 +145,18 @@ public class RegisterFrame extends JFrame {
 		});
 
 	}
-
+	
+	// 유효한 사용자 정보를 DB에 추가하고 회원가입을 완료한다.
 	void Register(String name, String phone, String passwd, String gender, String height, String weight,
 			String branch) {
 		try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + DB2022Team07_main.DBID,
 				DB2022Team07_main.USERID, DB2022Team07_main.PASSWD); Statement stmt = conn.createStatement();) {
-			// ȸ���� �̸��� ��ȭ��ȣ�� �������� member_id �˻�
+			// member table에 tuple를 하나 삽입한다.
 			PreparedStatement pStmt = conn.prepareStatement("insert into DB2022_members values(?,?,?,?,?,?,?,?,?)");
 			ResultSet rSet = stmt.executeQuery("select count(*) as num from DB2022_members;");
 
 			if (rSet.next()) {
-				int n = rSet.getInt("num") + 1;
+				int n = rSet.getInt("num") + 1; // 총 회원 수에 1을 더하여 member_id로 지정한다.
 				System.out.println(n);
 				pStmt.setInt(1, n);
 				pStmt.setString(2, name);
@@ -160,6 +168,7 @@ public class RegisterFrame extends JFrame {
 				pStmt.setString(8, branch);
 				pStmt.setString(9, null);
 				pStmt.executeUpdate();
+				// 회원가입 완료 메시지 띄우기
 				label.setText("<html><body style='text-align:center;'>"
 						+ "----------------------------------------------------------------------------------<br/>"
 						+ "Your registration is completed<br/>"
